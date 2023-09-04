f8197e4a9f434ebc37326d52a6f12c72b32b96f7
Plotnikov Alexander <alex@jobrobotix.com>
2023-09-04 11:25:30 +0200

    added #67

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,42 +1,54 @@
 ﻿using Leetcode.LinkedList;
+using Leetcode.String;
+
+#region Array
+
+#endregion
 
 #region Linked List
 
-// #2 https://leetcode.com/problems/add-two-numbers/
-AddTwoNumbers_2.Check();
+//// #2 https://leetcode.com/problems/add-two-numbers/
+//AddTwoNumbers_2.Check();
 
-// #19 https://leetcode.com/problems/remove-nth-node-from-end-of-list/
-RemoveNthNodeFromEndOfList_19.Check();
+//// #19 https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+//RemoveNthNodeFromEndOfList_19.Check();
 
-// #21 https://leetcode.com/problems/merge-two-sorted-lists/
-MergeTwoSortedLists_21.Check();
+//// #21 https://leetcode.com/problems/merge-two-sorted-lists/
+//MergeTwoSortedLists_21.Check();
 
-// #61 https://leetcode.com/problems/rotate-list/
-RotateList_0061.Check();
+//// #61 https://leetcode.com/problems/rotate-list/
+//RotateList_0061.Check();
 
-// #82 https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
-RemoveDuplicatesFromSortedListII_82.Check();
+//// #82 https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+//RemoveDuplicatesFromSortedListII_82.Check();
 
-// #83 https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-RemoveDuplicatesFromSortedList_83.Check();
+//// #83 https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+//RemoveDuplicatesFromSortedList_83.Check();
 
-// #143 https://leetcode.com/problems/reorder-list/
-ReorderList_143.Check();
+//// #143 https://leetcode.com/problems/reorder-list/
+//ReorderList_143.Check();
 
-// #203 https://leetcode.com/problems/remove-linked-list-elements/
-RemoveLinkedListElements_203.Check();
+//// #203 https://leetcode.com/problems/remove-linked-list-elements/
+//RemoveLinkedListElements_203.Check();
 
-// #206 https://leetcode.com/problems/reverse-linked-list/
-ReverseLinkedList_206.Check();
+//// #206 https://leetcode.com/problems/reverse-linked-list/
+//ReverseLinkedList_206.Check();
 
-// #876 https://leetcode.com/problems/middle-of-the-linked-list
-MiddleOfTheLinkedList_876.Check();
+//// #876 https://leetcode.com/problems/middle-of-the-linked-list
+//MiddleOfTheLinkedList_876.Check();
 
-// #1290 https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
-ConvertBinaryNumberInALinkedListToInteger_1290.Check();
+//// #1290 https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+//ConvertBinaryNumberInALinkedListToInteger_1290.Check();
 
-// #2816 https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/
-DoubleANumberRepresentedAsALinkedList_2816.Check();
+//// #2816 https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/
+//DoubleANumberRepresentedAsALinkedList_2816.Check();
+
+#endregion
+
+#region String
+
+//// #67 https://leetcode.com/problems/add-binary/
+//AddBinary_67.Check();
 
 #endregion
 
---
+++ Leetcode/String/0067_AddBinary.cs
@@ -0,0 +1,101 @@
+﻿using Leetcode.LinkedList.Model;
+using Leetcode.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.String
+{
+    internal class AddBinary_67
+    {
+        public static string AddBinary(string a, string b)
+        {
+            char[] aa, bb;
+            if (a.Length >= b.Length)
+            {
+                aa = a.ToCharArray();
+                bb = b.ToCharArray();
+            }
+            else
+            {
+                aa = b.ToCharArray();
+                bb = a.ToCharArray();
+            }
+
+            Array.Reverse(aa);
+            Array.Reverse(bb);
+
+            a = "";
+            bool overflow = false;
+            for (int i = 0; i < aa.Count(); i++)
+            {
+                char temp = (i < bb.Count()) ? bb[i] : '0';
+
+                if ((aa[i] == '1' && temp == '1') && !overflow)
+                {
+                    a += '0';
+                    overflow = true;
+                }
+                else if ((aa[i] == '1' && temp == '1') && overflow
+                    || (aa[i] == '1' && temp == '0' || aa[i] == '0' && temp == '1') && !overflow)
+                {
+                    a += '1';
+                }
+                else if ((aa[i] == '1' && temp == '0' || aa[i] == '0' && temp == '1') && overflow
+                        || aa[i] == '0' && temp == '0' && !overflow)
+                {
+                    a += '0';
+                }
+                else if (aa[i] == '0' && temp == '0' && overflow)
+                {
+                    a += '1';
+                    overflow = false;
+                }
+            }
+
+            if (overflow)
+                a += '1';
+
+            aa = a.ToCharArray();
+            Array.Reverse(aa);
+
+            return new string(aa);
+        }
+
+        public static void Check()
+        {
+            var test1 = new TestData_67()
+            {
+                Input1 = "11",
+                Input2 = "1",
+                Output = "100"
+            };
+            if (!CheckResult(test1))
+                throw new Exception("Error test 1");
+
+            var test2 = new TestData_67()
+            {
+                Input1 = "1010",
+                Input2 = "1011",
+                Output = "10101"
+            };
+            if (!CheckResult(test2))
+                throw new Exception("Error test 2");
+        }
+
+        private static bool CheckResult(TestData_67 data)
+        {
+            string res = AddBinary(data.Input1, data.Input2);
+            return res == data.Output;
+        }
+    }
+
+    internal class TestData_67
+    {
+        public string Input1 { get; set; }
+        public string Input2 { get; set; }
+        public string Output { get; set; }
+    }
+}
