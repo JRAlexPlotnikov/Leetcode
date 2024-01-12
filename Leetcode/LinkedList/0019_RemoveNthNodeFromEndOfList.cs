fb77200d4ee2fedfcc3cfb04500af70267eb59c3
Plotnikov Alexander <alex@jobrobotix.com>
2024-01-12 10:55:17 +0100

    update #19

--- Leetcode/LinkedList/0019_RemoveNthNodeFromEndOfList.cs
+++ Leetcode/LinkedList/0019_RemoveNthNodeFromEndOfList.cs
@@ -6,31 +6,71 @@ namespace Leetcode.LinkedList
     {
         public static ListNode? RemoveNthFromEnd(ListNode? head, int n)
         {
-            int length = 0;
-            ListNode? temp = head;
-            while (temp != null)
+            ListNode? revert = null;
+            while (head != null)
             {
-                length++;
-                temp = temp.next;
-            }
-
-            n = length - n;
-
-            return RemoveNthFromStart(head, n, 0);
-        }
-
-        private static ListNode? RemoveNthFromStart(ListNode? head, int n, int index)
-        {
-            ListNode? temp = null;
-            if (head != null && n == index)
+                revert = new ListNode(head.val, revert);
                 head = head.next;
-
-            if (head != null)
-            {
-                temp = new ListNode(head.val, RemoveNthFromStart(head.next, n, (index + 1)));
             }
-            return temp;
+
+            if (n == 1)
+            {
+                revert = revert?.next;
+            }
+            else
+            {
+                int i = 1;
+                head = revert;
+                while (head != null)
+                {
+                    if (i == n - 1)
+                    {
+                        head.next = head?.next?.next;
+                        break;
+                    }
+                    i++;
+                    head = head.next;
+                }
+            }
+
+            head = null;
+            while (revert != null)
+            {
+                head = new ListNode(revert.val, head);
+                revert = revert.next;
+            }
+
+            return head;
         }
+
+
+        //public static ListNode? RemoveNthFromEnd(ListNode? head, int n)
+        //{
+        //    int length = 0;
+        //    ListNode? temp = head;
+        //    while (temp != null)
+        //    {
+        //        length++;
+        //        temp = temp.next;
+        //    }
+
+        //    n = length - n;
+
+        //    return RemoveNthFromStart(head, n, 0);
+        //}
+
+        //private static ListNode? RemoveNthFromStart(ListNode? head, int n, int index)
+        //{
+        //    ListNode? temp = null;
+        //    if (head != null && n == index)
+        //        head = head.next;
+
+        //    if (head != null)
+        //    {
+        //        temp = new ListNode(head.val, RemoveNthFromStart(head.next, n, (index + 1)));
+        //    }
+        //    return temp;
+        //}
 
         public static void Check()
         {
@@ -41,7 +81,7 @@ namespace Leetcode.LinkedList
                 Output = new int[] { 1, 2, 3, 5 }
             };
             if (!CheckResult(test1))
-                throw new Exception("Error test 1");
+                throw new Exception("Error test");
 
             var test2 = new TestData_19()
             {
@@ -50,7 +90,7 @@ namespace Leetcode.LinkedList
                 Output = Array.Empty<int>()
             };
             if (!CheckResult(test2))
-                throw new Exception("Error test 2");
+                throw new Exception("Error test");
 
             var test3 = new TestData_19()
             {
@@ -59,7 +99,16 @@ namespace Leetcode.LinkedList
                 Output = new int[] { 1 }
             };
             if (!CheckResult(test3))
-                throw new Exception("Error test 3");
+                throw new Exception("Error test");
+
+            var test4 = new TestData_19()
+            {
+                Input = new int[] { 1, 2, 3 },
+                N = 3,
+                Output = new int[] { 2, 3 }
+            };
+            if (!CheckResult(test4))
+                throw new Exception("Error test");
         }
 
         private static bool CheckResult(TestData_19 data)
